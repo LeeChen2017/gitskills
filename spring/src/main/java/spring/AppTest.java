8ac9763e654b3ce2b7bf0938ec0070acc565a571
李纯 <@192.168.137.1>
2020-06-28 00:02:13 +0800

    [feat][create new FileTest method]

--- spring/src/main/java/spring/AppTest.java
+++ spring/src/main/java/spring/AppTest.java
@@ -1,5 +1,31 @@
 package spring;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
 public class AppTest {
+	
+	public static void test1() {
+		
+		File file = new File("D://test.txt");
+		
+		file.exists();
+		
+		try {
+			String str = FileUtils.readFileToString(file , "ISO-8859-1");
+			System.out.println(str);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	public static void main(String[] args) {
+		
+		test1();
+	}
 
 }
